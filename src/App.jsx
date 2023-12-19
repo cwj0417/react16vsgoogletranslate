a97b2fa648a6da31680182cc39ef1d8ba41cdf16
cwj0417 <chenwenjun@yo-cwj.com>
2023-12-19 11:36:04 +0800

    feat: mvp

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,26 @@ import { Component } from 'react'
 
 class App extends Component {
   state = {
-    visible: false,
-    info: {
-      info1: '哈哈',
-      info2: '呵呵'
-    }
+    visible: false
   }
 
   go = () => {
     this.setState({
       visible: true,
-      info: null
-    })
-    setTimeout(() => {
-      this.setState({
-        info: {
-          info1: '1',
-          info2: '2'
-        }
-      })
     })
   }
 
   render() {
-    const { visible, info } = this.state;
+    const { visible } = this.state;
     return (
       <div>
         <button onClick={this.go}>中文</button>
-        中文中文
         {visible && (
           <div>
-            中文啊啊
-            {info?.info1}
-            中文哦哦
-            {info?.info2}
-            中文嗯嗯
+            哈哈
           </div>
         )}
+        中文中文
       </div>
     )
   }
